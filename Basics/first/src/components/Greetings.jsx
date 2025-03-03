64f56becbda22f0a382cb3fa44dcda966f452932
balakumaran-ks <balacodespace@gmail.com>
2025-03-03 20:17:18 +0530

    Created my first react component

---
+++ Basics/first/src/components/Greetings.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Greeting() {
+  return (
+    <>
+      <h2>Greetings from Balakumaran , this is a react component...</h2>
+    </>
+  );
+}
+
+export default Greeting;
