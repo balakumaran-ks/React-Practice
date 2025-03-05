0dedc09d62df529aca62f321f69d2e253eefe43f
balakumaran-ks <balacodespace@gmail.com>
2025-03-05 11:34:53 +0530

    Modified the component

--- Basics/first/src/components/Greetings.jsx
+++ Basics/first/src/components/Greetings.jsx
@@ -3,9 +3,21 @@ import React from "react";
 function Greeting() {
   return (
     <>
-      <h2>Greetings from Balakumaran , this is a react component...</h2>
+      {/* <h2>Greetings from Balakumaran , this is a react component...</h2> */}
+      <h2>{greet()} from Balakumaran , this is a react component...</h2>
     </>
   );
+}
+
+function greet() {
+  let now = new Date().getHours();
+  if (now < 12) {
+    return "Good Morning";
+  } else if (now < 4) {
+    return "Good Afternoon";
+  } else {
+    return "Goog Evening";
+  }
 }
 
 export default Greeting;
